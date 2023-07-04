f22443df9573bfc64402fb059759fa6c09ba8b11
FelipeAviani <felipeaviani@gmail.com>
2023-07-04 15:53:43 -0300

    cambios en la carga de la página

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { lazy, Suspense } from "react";
-import { Helmet } from 'react-helmet'
+import { lazy, Suspense, useEffect } from "react";
+import { Helmet } from 'react-helmet';
 import Loading from "./components/loading/Loading.jsx";
 import NavBar from "./components/navbar/NavBar.jsx";
 import ScrollToTop from "./components/scrolltotop/ScrollToTop.jsx";
@@ -12,21 +12,32 @@ const Contact = lazy(() => import("./components/contact/Contact.jsx"));
 const Soluciones = lazy(() => import("./components/soluciones/Soluciones.jsx"));
 
 function App() {
+  useEffect(() => {
+    // Mueve la carga del archivo CSS de font-awesome al componente App para que solo se cargue en las rutas necesarias.
+    const link = document.createElement('link');
+    link.href = "https://use.fontawesome.com/releases/v5.15.1/css/all.css";
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
     <Router>
+      <Helmet>
+        <link rel="canonical" href={window.location.href} />
+      </Helmet>
       <NavBar />
       <Suspense fallback={<Loading />}>
         <ScrollToTop />
         <Switch>
-        <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/nosotros" component={Nosotros} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/services" component={Soluciones} />
           <Route path="*" component={NotFound} />
         </Switch>
-        <Helmet>
-          <link rel="canonical" href={window.location.href} />
-        </Helmet>
       </Suspense>
     </Router>
   );
